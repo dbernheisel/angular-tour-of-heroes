da6902bb0ce25edc3df7d58e63709ab9fe15438f
David Bernheisel <david@bernheisel.com>
2016-04-08 08:15:59 -0400

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component }           from 'angular2/core';
+import { RouteConfig,
+         ROUTER_DIRECTIVES,
+         ROUTER_PROVIDERS }    from 'angular2/router';
+import { HeroService }         from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { HeroDetailComponent }     from './hero-detail.component';
+import { DashboardComponent }  from './dashboard.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  directives: [
+    ROUTER_DIRECTIVES
+  ],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
+})
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }
+])
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
